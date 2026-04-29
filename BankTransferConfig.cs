421b4b6218c0b4f5ee0144661fa6fff7a89c8f49
Mri-cia <vyarnamricia@gmail.com>
2026-04-29 07:48:06 +0700

    penambahan design by contract pada semua kelas

---
+++ BankTransferConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class BankTransferConfig
+{
+    [JsonIgnore]
+    private string _filepath = Path.Combine(Environment.CurrentDirectory, "bank_transfer_config.json");
+
+    public class Transfer
+    {
+        [JsonPropertyName("lang")]
+        public string Language { get; set; }
+        [JsonPropertyName("transfer")]
+        public TransferThreshold TransferThrs { get; set; }
+        [JsonPropertyName("methods")]
+        public string[] Methods { get; set; }
+        [JsonPropertyName("confirmation")]
+        public ConfirmationMessage ConfirmationMessage { get; set; }
+    }
+
+    public class TransferThreshold
+    {
+        [JsonPropertyName("threshold")]
+        public int ThresholdLimit { get; set; }
+        [JsonPropertyName("low_fee")]
+        public int low_fee { get; set; }
+        [JsonPropertyName("high_fee")]
+        public int high_fee { get; set; }
+    }
+
+    public class ConfirmationMessage
+    {
+        [JsonPropertyName("en")]
+        public string English { get; set; }
+        [JsonPropertyName("id")]
+        public string Indonesian { get; set; }
+    }
+
+    public Transfer transferObj { get; set; }
+
+    public void LoadConfig()
+    {
+        if (File.Exists(_filepath))
+        {
+            string jsonString = File.ReadAllText(_filepath);
+            var config = JsonSerializer.Deserialize<Transfer>(jsonString);
+            this.transferObj = config;
+        }
+        else
+        {
+            Console.WriteLine("Configuration file not found.");
+            transferObj = new Transfer
+            {
+                Language = "en",
+                TransferThrs = new TransferThreshold { ThresholdLimit = 1000000, low_fee = 5000, high_fee = 10000 },
+                Methods = ["RTO (real-time)", "SKN", "RTGS", "BI FAST"],
+                ConfirmationMessage = new ConfirmationMessage { English = "Are you sure you want to transfer?", Indonesian = "Apakah Anda yakin ingin mentransfer?" }
+            };
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,117 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var transferConfig = new BankTransferConfig();
+
+transferConfig.LoadConfig();
+
+var config = transferConfig.transferObj;
+
+if (config.Language == "en")
+{
+    Console.WriteLine("Please insert the amount of money to transfer:");
+}
+else if (config.Language == "id")
+{
+    Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+}
+else
+{
+    Console.WriteLine("Unsupported language.");
+    return ;
+}
+
+int amount = int.Parse(Console.ReadLine());
+int transferFee;
+var threshold = config.TransferThrs;
+
+Console.WriteLine($"");
+
+if (amount <= threshold.ThresholdLimit)
+{
+    if (config.Language == "en")
+    {
+        Console.WriteLine($"Transfer fee: {threshold.low_fee}");
+    }
+    else if (config.Language == "id")
+    {
+        Console.WriteLine($"Biaya transfer: {threshold.low_fee}");
+    }
+
+    transferFee = threshold.low_fee;
+}
+else
+{
+    if (config.Language == "en")
+    {
+        Console.WriteLine($"Transfer fee: {threshold.high_fee}");
+    }
+    else if (config.Language == "id")
+    {
+        Console.WriteLine($"Biaya transfer: {threshold.high_fee}");
+    }
+
+    transferFee = threshold.high_fee;
+}
+
+Console.WriteLine($"");
+
+if (config.Language == "en")
+{
+    Console.WriteLine("Select Transfer Method: ");
+}
+else if (config.Language == "id")
+{
+    Console.WriteLine("Pilih Metode Transfer");
+}
+
+for (int i = 0; i < config.Methods.Length; i++)
+{
+    Console.WriteLine($"{i + 1}. {config.Methods[i]}");
+}
+
+Console.WriteLine($"");
+
+if (config.Language == "en")
+{
+    Console.WriteLine("Input type:");
+}
+else if (config.Language == "id")
+{
+    Console.WriteLine("Ketik input:");
+}
+
+int methodChoice = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"");
+
+if(config.Language == "en")
+{
+    Console.WriteLine($"Please type {config.ConfirmationMessage.English} to confirm transaction:");
+}
+else if (config.Language == "id")
+{
+    Console.WriteLine($"Ketik {config.ConfirmationMessage.Indonesian} untuk mengkonfirmasi transaksi:");
+}
+
+string confirmation = Console.ReadLine();
+
+if (config.Language == "en")
+{
+    if (confirmation == config.ConfirmationMessage.English)
+    {
+        Console.WriteLine($"The transfer is completed using {config.Methods[methodChoice-1]}");
+    }
+    else
+    {
+        Console.WriteLine("Transfer is cancelled.");
+    }
+}
+else if (config.Language == "id")
+{
+    if (confirmation == config.ConfirmationMessage.Indonesian)
+    {
+        Console.WriteLine($"Proses transfer berhasil menggunakan {config.Methods[methodChoice-1]}");
+    }
+    else
+    {
+        Console.WriteLine("Transfer dibatalkan.");
+    }
+}
